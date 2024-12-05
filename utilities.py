e69dc0f683d236b808a6478f109bb6c70e0f37fc
Stanislas Ducotterd <ducotter@gpu213.ds-a4-r08.cct.rcp.epfl.ch>
2024-12-05 19:36:21 +0100

    added utilities and removed uselss plotting files

--- utilities.py
+++ utilities.py
@@ -24,3 +24,46 @@ def batch_SSIM(img, imclean, data_range):
     for i in range(Img.shape[0]):
         SSIM += compare_ssim(Iclean[i,:,:,:], Img[i,:,:,:], data_range=data_range, multichannel=True)
     return SSIM/Img.shape[0]
+
+def transform(x, iter):
+    if iter % 8 == 1:
+        return torch.flip(x, [2])
+    elif iter % 8 == 2:
+        return torch.flip(x, [3])
+    elif iter % 8 == 3:
+        return torch.rot90(x, 1, [2, 3])
+    elif iter % 8 == 4:
+        return torch.rot90(x, 2, [2, 3])
+    elif iter % 8 == 5:
+        return torch.rot90(x, 3, [2, 3])
+    elif iter % 8 == 6:
+        return torch.flip(torch.rot90(x, 1, [2, 3]), [2])
+    elif iter % 8 == 7:
+        return torch.flip(torch.rot90(x, 1, [2, 3]), [3])
+    else:
+        return x
+    
+def inverse(x, iter):
+    if iter % 8 == 1:
+        return torch.flip(x, [2])
+    elif iter % 8 == 2:
+        return torch.flip(x, [3])
+    elif iter % 8 == 3:
+        return torch.rot90(x, 3, [2, 3])
+    elif iter % 8 == 4:
+        return torch.rot90(x, 2, [2, 3])
+    elif iter % 8 == 5:
+        return torch.rot90(x, 1, [2, 3])
+    elif iter % 8 == 6:
+        return torch.rot90(torch.flip(x, [2]), 3, [2, 3])
+    elif iter % 8 == 7:
+        return torch.rot90(torch.flip(x, [3]), 3, [2, 3])
+    else:
+        return x
+    
+def center_crop(data, shape):
+    w_from = (data.shape[-2] - shape[0]) // 2
+    h_from = (data.shape[-1] - shape[1]) // 2
+    w_to = w_from + shape[0]
+    h_to = h_from + shape[1]
+    return data[..., w_from:w_to, h_from:h_to]
